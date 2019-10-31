81823570ead8b53b70211ca3289b82ac35124556
lsonerose <2665567025@qq.com>
2019-10-31 15:43:38 +0800

    复制demo的app_key\app_secret\pack_id 依赖第三方jar包 添加权限 添加混淆代码

---
+++ app/src/main/java/com/example/rose/himalaya/TingApplication.java
@@ -0,0 +1,59 @@
+package com.example.rose.himalaya;
+
+import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+
+import com.ximalaya.ting.android.opensdk.auth.constants.XmlyConstants;
+import com.ximalaya.ting.android.opensdk.constants.ConstantsOpenSdk;
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.AccessTokenManager;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.httputil.XimalayaException;
+import com.ximalaya.ting.android.opensdk.player.appnotification.XmNotificationCreater;
+import com.ximalaya.ting.android.opensdk.util.BaseUtil;
+import com.ximalaya.ting.android.opensdk.util.Logger;
+import com.ximalaya.ting.android.player.XMediaPlayerConstants;
+import com.ximalaya.ting.android.sdkdownloader.XmDownloadManager;
+import com.ximalaya.ting.android.sdkdownloader.http.RequestParams;
+import com.ximalaya.ting.android.sdkdownloader.http.app.RequestTracker;
+import com.ximalaya.ting.android.sdkdownloader.http.request.UriRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+/**
+ * Created by le.xin on 2016/6/12.
+ */
+public class TingApplication extends Application {
+    public static final String REFRESH_TOKEN_URL = "https://api.ximalaya.com/oauth2/refresh_token?";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CommonRequest mXimalaya = CommonRequest.getInstanse();
+        if(DTransferConstants.isRelease) {
+            String mAppSecret = "8646d66d6abe2efd14f2891f9fd1c8af";
+            mXimalaya.setAppkey("9f9ef8f10bebeaa83e71e62f935bede8");
+            mXimalaya.setPackid("com.app.test.android");
+            mXimalaya.init(this ,mAppSecret);
+        } else {
+            String mAppSecret = "0a09d7093bff3d4947a5c4da0125972e";
+            mXimalaya.setAppkey("f4d8f65918d9878e1702d49a8cdf0183");
+            mXimalaya.setPackid("com.ximalaya.qunfeng");
+            mXimalaya.init(this ,mAppSecret);
+        }
+    }
+}
