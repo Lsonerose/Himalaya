f1a14e91bf42be9a6f2e875e71824f2b5c730609
lsonerose <2665567025@qq.com>
2019-11-02 19:18:23 +0800

    '绑定ViewPager与Indicator'

---
+++ app/src/main/java/com/example/rose/himalaya/fragments/SubscriptionFragment.java
@@ -0,0 +1,10 @@
+package com.example.rose.himalaya.fragments;
+
+import com.example.rose.himalaya.base.BaseFragment;
+
+/**
+ * Created by rose on 2019/11/2.
+ */
+
+public class SubscriptionFragment extends BaseFragment {
+}
---
+++ app/src/main/java/com/example/rose/himalaya/utils/FragmentCreator.java
@@ -0,0 +1,8 @@
+package com.example.rose.himalaya.utils;
+
+/**
+ * Created by rose on 2019/11/2.
+ */
+
+public class FragmentCreator {
+}
