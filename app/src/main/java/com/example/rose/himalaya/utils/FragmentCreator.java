f23a686dd50c879cc1f0086d3102cd05a9f7590a
lsonerose <2665567025@qq.com>
2019-11-04 09:19:25 +0800

    '添加注释'

--- app/src/main/java/com/example/rose/himalaya/utils/FragmentCreator.java
+++ app/src/main/java/com/example/rose/himalaya/utils/FragmentCreator.java
@@ -15,7 +15,7 @@ import java.util.Map;
  */
 
 public class FragmentCreator {
-    private static Map<Integer,BaseFragment> cache = new HashMap<>();
+    private static Map<Integer,BaseFragment> cache = new HashMap<>();//保存以及创建过的Fragment、避免浪费资源
 
     private final static int INDEX_RECOMMENT = 0;
     private final static int INDEX_SUBSCRIPTION = 1;
@@ -25,10 +25,10 @@ public class FragmentCreator {
 
     public static BaseFragment getFragment(int index){
         BaseFragment baseFragment = cache.get(index);
-        if (baseFragment != null)
+        if (baseFragment != null)//判断集合里面是否有值 有的话就直接返回集合里面的内容 没有则继续往下走
             return baseFragment;
 
-        switch (index){
+        switch (index){//创建Fragment内容
             case INDEX_HISTORY:
                 baseFragment = new HistoryFragment();
                 break;
@@ -39,7 +39,7 @@ public class FragmentCreator {
                 baseFragment = new SubscriptionFragment();
                 break;
         }
-        cache.put(index,baseFragment);
-        return baseFragment;
+        cache.put(index,baseFragment);//存到集合里面去
+        return baseFragment;//返回创建的内容
     }
 }
